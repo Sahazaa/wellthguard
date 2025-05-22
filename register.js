0643e6cf4b597965708a1c8d75fad4dab886711d
Saahaza <sahaza.nir@gmail.com>
2025-05-22 13:37:04 +0300

    modifications

---
+++ register.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBfnsxtNpcTj4oGFewKACtFhvyGyVnDp_4",
+    authDomain: "login-authentification-d69f7.firebaseapp.com",
+    projectId: "login-authentification-d69f7",
+    storageBucket: "login-authentification-d69f7.firebasestorage.app",
+    messagingSenderId: "281243985617",
+    appId: "1:281243985617:web:42f1ddd28d86d076a947f0"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
